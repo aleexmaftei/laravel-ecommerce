2e103b74adeef170e017772ac4f5212341d380d2
Alex Maftei <alex.maftei99@gmail.com>
2024-02-08 18:06:57 +0200

    Refactor routes

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,6 @@ use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 
-
 Route::controller(CategoryController::class)->group(function () {
     Route::get("/", [CategoryController::class, "index_category"])->name("home");
 
@@ -14,17 +13,17 @@ Route::controller(CategoryController::class)->group(function () {
         ->name("category.show_subcategories");
 });
 
-Route::controller(ProductController::class)->group(function () {
-    Route::get("/category/{category_id}/products", "index")
+Route::controller(ProductController::class)->prefix("/category/{category_id}/products")->group(function () {
+    Route::get("/", "index")
         ->where("category_id", "\d+")
         ->name("products.index");
 
-    Route::get("/category/{category_id}/products/edit/{product_id}", "edit")
+    Route::get("/edit/{product_id}", "edit")
         ->where("category_id", "\d+")
         ->where("product_id", "\d+")
         ->name("products.edit");
 
-    Route::put("/category/{category_id}/products/edit/{product_id}", "update")
+    Route::put("/edit/{product_id}", "update")
         ->where("category_id", "\d+")
         ->where("product_id", "\d+")
         ->name("products.update");
