e4d8bae7ab526c0dd643a48482567bf166d8bbaf
Alex Maftei <alex.maftei99@gmail.com>
2024-02-11 00:42:57 +0200

    More seed for delivery locations

--- database/seeders/DeliveryLocationSeeder.php
+++ database/seeders/DeliveryLocationSeeder.php
@@ -12,6 +12,6 @@ class DeliveryLocationSeeder extends Seeder
      */
     public function run(): void
     {
-        DeliveryLocation::factory()->count(50)->create();
+        DeliveryLocation::factory()->count(1000)->create();
     }
 }
